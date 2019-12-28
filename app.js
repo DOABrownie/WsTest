17b58bd753b7f9258d2d5c58897bea9b7ad5ea5b
DOABrownie <42855093+DOABrownie@users.noreply.github.com>
2019-12-29 00:02:09 +1300

    3
    3

---
+++ app.js
@@ -0,0 +1,14 @@
+
+
+
+const Get_Balance_Div = document.getElementById("gb");
+const Get_Ticker_Div = document.getElementById("gt");
+
+function
+    Get_Balance_Div.addEventListener('click', function() {
+        message("gb");
+    })
+
+    Get_Ticker_Div.addEventListener('click', function() {
+        message("gt");
+    })
